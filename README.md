962cf77249fa3a30f850f4782b4b9ca7a47fe997
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2021-06-10 10:16:19 +0700

    init project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # demo-rmi
+start rmi server
+```shell
+rmiregistry
+```
+run Server.java to regis to server
+
+run Client.java to call to server to invoke 
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,7 +1,9 @@
+import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.Objects;
 
 public class Server implements Hello {
     @Override
@@ -12,8 +14,11 @@ public class Server implements Hello {
     public static void main(String[] args) {
         try {
             Server server = new Server();
-            Hello stub = (Hello) UnicastRemoteObject.exportObject(server, 3099);
+            Hello stub = (Hello) UnicastRemoteObject.exportObject(server, 0);
             Registry registry = LocateRegistry.getRegistry();
+            Remote obj = registry.lookup("Hello");
+            if (Objects.nonNull(obj))
+                registry.unbind("Hello");
             registry.bind("Hello", stub);
             System.out.println("system ready");
         } catch (Throwable throwable) {
