30f527b035775ad928e4c3b9436133162232a84d
nguyenhuuluan434 <nguyenhuuluan434@gmail.com>
2021-06-10 13:29:50 +0700

    add comments

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -13,12 +13,13 @@ public class Server implements Hello {
 
     public static void main(String[] args) {
         try {
+            //create object what will regis into rmi server
             Server server = new Server();
+            //create stub listen on random port to serve when client connect to
             Hello stub = (Hello) UnicastRemoteObject.exportObject(server, 0);
-            Registry registry = LocateRegistry.getRegistry();
-            Remote obj = registry.lookup("Hello");
-            if (Objects.nonNull(obj))
-                registry.unbind("Hello");
+            //get registry to regis stub
+            Registry registry = LocateRegistry.getRegistry("127.0.0.1",1099);
+            //regis stub to registry, when client connect to registry and resolve the stub will connect to stub base on info of registry
             registry.bind("Hello", stub);
             System.out.println("system ready");
         } catch (Throwable throwable) {
